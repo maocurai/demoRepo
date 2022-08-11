ed4b72bf93f8ee81657dd10580f2696e55d51901
Maryna Piatnytsia <mpatnica924@gmail.com>
2022-08-11 22:46:53 +0300

    user model added

---
+++ demoApp/demoApp/src/main/java/com/example/demoapp/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demoapp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
