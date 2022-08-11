329431036d862fdc98733d2ecbada7b87e1940d0
Maryna Piatnytsia <mpatnica924@gmail.com>
2022-08-11 22:49:55 +0300

    start method added

--- demoApp/demoApp/src/main/java/com/example/demoapp/controller/MainController.java
+++ demoApp/demoApp/src/main/java/com/example/demoapp/controller/MainController.java
@@ -1,4 +1,14 @@
 package com.example.demoapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String start() {
+        return "Hello world";
+    }
+
 }
