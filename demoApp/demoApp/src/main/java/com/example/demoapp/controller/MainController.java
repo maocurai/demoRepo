2a26269e2312b7bf1792123ac4e0f17e03a1b347
Maryna Piatnytsia <mpatnica924@gmail.com>
2022-08-11 23:00:28 +0300

    comment added

--- demoApp/demoApp/src/main/java/com/example/demoapp/controller/MainController.java
+++ demoApp/demoApp/src/main/java/com/example/demoapp/controller/MainController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    //start page mapping
     @GetMapping("/")
     public String start() {
         return "Hello world";
     }
+
 
 }
